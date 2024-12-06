a7810d51af1ef71b1fbfb6896a11b75e8478030c
Paulo <pmatos2000@gmail.com>
2024-12-06 20:38:21 -0300

    Primeira Controller

---
+++ Backend/ToDo.API/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo.Repositories.Model.Entity;
+
+namespace ToDo.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(User user)
+        {
+            return Ok();
+        }
+    }
+}
