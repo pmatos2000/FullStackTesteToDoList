60f941de888abc56eeed42da44c44449f07eadae
Paulo <pmatos2000@gmail.com>
2024-12-06 14:57:59 -0300

    Add Relacionamento

---
+++ Backend/ToDo.API/Migrations/20241206175211_AddRelacionamento.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRelacionamento : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_tasks_user_id",
+                table: "tasks",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_tasks_users_user_id",
+                table: "tasks",
+                column: "user_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_tasks_users_user_id",
+                table: "tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_tasks_user_id",
+                table: "tasks");
+        }
+    }
+}
