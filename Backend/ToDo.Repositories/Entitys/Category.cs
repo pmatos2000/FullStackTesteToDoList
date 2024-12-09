b4c5f3297c374e2d7cf9952723b58f0e149f7341
Paulo <pmatos2000@gmail.com>
2024-12-09 18:59:54 -0300

    Criação tabela categoria

---
+++ Backend/ToDo.Repositories/Entitys/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Repositories.Model;
+
+namespace ToDo.Repositories.Entitys
+{
+    public record Category
+    {
+        public long Id { get; init; }
+        public required string Name {  get; init; } 
+        public long UserId { get; init; }
+        public User? User { get; init; }
+        public DateTime CreatedAt { get; init; }
+        public DateTime UpdatedAt { get; init; }
+        public ICollection<TodoItem>? Tasks { get; init; }
+    }
+}
