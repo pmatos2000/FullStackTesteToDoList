1c96323a35c568db51549714aa78d0d9af063fb8
Paulo <pmatos2000@gmail.com>
2024-12-10 20:11:50 -0300

    Tela login

--- Frontend/app/src/App.tsx
+++ Frontend/app/src/App.tsx
@@ -1,8 +1,20 @@
 import { FC } from "react";
 import Login from "./pages/Login";
+import { Box, styled } from "@mui/material";
+
+const Container = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+});
 
 const App: FC = () => {
-  return <Login></Login>;
+  return (
+    <Container>
+      <Login></Login>
+    </Container>
+  );
 };
 
 export default App;
--- Frontend/app/src/pages/Login.tsx
+++ Frontend/app/src/pages/Login.tsx
@@ -1,5 +1,88 @@
-import { FC } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  styled,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { FC, FormEvent } from "react";
 
-const Login: FC = () => <div>Olá mundo</div>;
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+const LoginCard = styled(Card)({
+  minWidth: "275px",
+  maxWidth: "275px",
+  padding: "24px",
+});
+
+const LoginAvatar = styled(Avatar)({
+  margin: "8px auto",
+  backgroundColor: "#f50057",
+});
+
+const LoginForm = styled("form")({
+  width: "100%",
+  marginTop: "8px",
+});
+
+const LoginSubmit = styled(Button)({
+  margin: "24px 0 16px",
+});
+
+const Login: FC = () => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(event.currentTarget.elements);
+  };
+  return (
+    <LoginCard>
+      <CardContent>
+        <Box textAlign="center">
+          <LoginAvatar>
+            <LockOutlinedIcon />
+          </LoginAvatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+        </Box>
+        <LoginForm onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="userName"
+            label="Nome usuário"
+            name="userName"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <LoginSubmit
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="login-submit"
+          >
+            Entrar
+          </LoginSubmit>
+        </LoginForm>
+      </CardContent>
+    </LoginCard>
+  );
+};
 
 export default Login;
