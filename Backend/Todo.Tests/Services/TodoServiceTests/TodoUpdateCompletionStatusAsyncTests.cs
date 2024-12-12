7f974bec8fcb18947e551a41e46a62bd6bd4b030
Paulo <pmatos2000@gmail.com>
2024-12-12 02:38:49 -0300

    TodoUpdateCompletionStatusAsyncTests

---
+++ Backend/Todo.Tests/Services/TodoServiceTests/TodoUpdateCompletionStatusAsyncTests.cs
@@ -0,0 +1,47 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Repositories.Interfaces;
+using ToDo.Services.Services;
+using Todo.Tests.Mocks;
+
+namespace Todo.Tests.Services.TodoServiceTests
+{
+    public class TodoUpdateCompletionStatusAsyncTests
+    {
+        private Mock<ITodoRepositorie> mockTodoRepositorie;
+
+        private TodoService todoService;
+
+        [SetUp]
+        public void Init()
+        {
+            mockTodoRepositorie = new Mock<ITodoRepositorie>();
+            todoService = new TodoService(mockTodoRepositorie.Object);
+        }
+
+        [Test]
+        public async Task UpdateSucess()
+        {
+            var userId = MockConstants.USER_ID_ONE;
+            var todoId = MockConstants.TASK_ID_ONE;
+            var isCompleted = MockConstants.TASK_IS_COMPLETED_ONE;
+
+            mockTodoRepositorie
+                .Setup(m => m.TodoUpdateCompletionStatusAsync(It.IsAny<long>(), It.IsAny<long>(), It.IsAny<bool>()))
+                .ReturnsAsync(MockConstants.TASK_ID_ONE);
+
+            var result = await todoService.TodoUpdateCompletionStatusAsync(userId, todoId, isCompleted);
+
+            Assert.That(result, Is.EqualTo(MockConstants.TASK_ID_ONE));
+
+            mockTodoRepositorie.Verify(
+                m => m.TodoUpdateCompletionStatusAsync(userId, todoId, isCompleted), Times.Once);
+
+            mockTodoRepositorie.VerifyNoOtherCalls();
+        }
+    }
+}
